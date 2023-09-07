9ab6853030c2dea2479f1c966d26c48b644d7844
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 17:54:08 +0200

    intermediatemanager

--- src/main/java/ba/unsa/etf/rpr/business/IntermediateManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/IntermediateManager.java
@@ -1,4 +1,23 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.IntermediateTable;
+import ba.unsa.etf.rpr.domain.Record;
+import ba.unsa.etf.rpr.exceptions.RecordException;
+
+import java.util.List;
+
 public class IntermediateManager {
+    public IntermediateTable add(IntermediateTable i) throws RecordException
+    {
+        return DaoFactory.intermediateDao().add(i);
+    }
+    public List<IntermediateTable> getAll() throws RecordException {
+        return DaoFactory.intermediateDao().getAll();
+    }
+
+    public List<IntermediateTable> getByUser(int idUser) throws RecordException {
+        return DaoFactory.intermediateDao().getByUser(idUser);
+    }
+
 }
