6efd328303e62a0eb2ee9f8ebf4011e7ec305fdf
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 14:59:52 +0200

    daofactory

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,4 +1,45 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.business.IntermediateManager;
+
+/**
+ * Factory method for singleton implementation of DAOs
+ *
+ * @author Ilma Husic
+ */
 public class DaoFactory {
+
+    private static final GenreDao genreDao = GenreDaoSQLImpl.getInstance();
+
+    private static final RecordDao recordDao = RecordDaoSQLImpl.getInstance();
+
+    private static final OrderDao orderDao = OrderDaoSQLImpl.getInstance();
+
+    private static final UserDao userDao = UserDaoSQLImpl.getInstance();
+
+    private static final IntermediateDao intermediateDao = IntermediateDaoSQLImpl.getInstance();
+
+    private DaoFactory(){
+    }
+
+    public static GenreDao genreDao(){
+        return genreDao;
+    }
+
+    public static RecordDao recordDao(){
+        return recordDao;
+    }
+
+    public static OrderDao orderDao(){
+        return orderDao;
+    }
+
+    public static UserDao userDao(){
+        return userDao;
+    }
+
+    public static IntermediateDao intermediateDao(){
+        return intermediateDao;
+    }
+
 }
