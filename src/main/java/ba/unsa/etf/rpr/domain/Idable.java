be9fb19aa8314986b46c102ae82406d695ca3e0d
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 18:01:06 +0200

    idable

--- src/main/java/ba/unsa/etf/rpr/domain/Idable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Idable.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr.domain;
 
-public class Idable {
+/**
+ * Interface that forces all POJO beans to have ID field.Name is stupid but per standard
+ */
+public interface Idable {
+
+    /**
+     * Setter for Id
+     * @param id int
+     */
+    void setId(int id);
+
+    /**
+     * Getter for Id
+     * @return int
+     */
+    int getId();
 }
