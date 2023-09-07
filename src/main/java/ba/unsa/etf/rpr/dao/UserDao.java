4a5fd93ae500262eb65993a4b98a54e4422ef486
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 17:45:52 +0200

    userdao

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr.dao;
 
-public class UserDao {
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.RecordException;
+
+/**
+ * Dao interface for Genre domain bean
+ * @author Ilma Husic
+ */
+public interface UserDao extends Dao<User> {
+    User findEmail(String emailField) throws RecordException;
+
 }
