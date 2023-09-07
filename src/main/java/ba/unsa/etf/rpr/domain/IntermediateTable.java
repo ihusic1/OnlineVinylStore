dc690efd1250585ac8013928faf6c9412bb946d0
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 18:02:23 +0200

    intermediatetable

--- src/main/java/ba/unsa/etf/rpr/domain/IntermediateTable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/IntermediateTable.java
@@ -1,4 +1,46 @@
 package ba.unsa.etf.rpr.domain;
 
-public class IntermediateTable {
+/**
+ *  This class is Intermediate table - one record can be in more than one order, and one order can contain more than one record
+ * @author Ilma Husic
+ **/
+public class IntermediateTable implements Idable {
+
+
+
+    private int id;
+    private Record record;
+    private Order order;
+
+
+    /**
+     * This class provides getters and setters for accessing and modifying the record and order associated with the intermediate table entry.
+     */
+    public Record getRecord() {
+        return record;
+    }
+
+    public void setRecord(Record record) {
+        this.record = record;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
