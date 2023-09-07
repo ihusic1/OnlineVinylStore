4c5a1908e2bf376d2e534cbd4a89d62fa64982a0
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 18:28:31 +0200

    contactcontroller added

--- src/main/java/ba/unsa/etf/rpr/controllers/ContactController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ContactController.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+
 public class ContactController {
+
+    public Button Undo;
+    public void closeWindow(ActionEvent actionEvent) {
+        Stage stage =(Stage)Undo.getScene().getWindow();
+        stage.close();
+    }
 }
