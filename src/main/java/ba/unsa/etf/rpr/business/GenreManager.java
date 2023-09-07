68134f57b7ee7be257414f87242c7d3da17fadbe
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 17:51:42 +0200

    genremanager

--- src/main/java/ba/unsa/etf/rpr/business/GenreManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/GenreManager.java
@@ -1,4 +1,13 @@
-package ba.unsa.etf.rpr.business;
+package ba.unsa.etf.rpr.exceptions;
 
-public class GenreManager {
+public class RecordException extends Exception{
+
+    public RecordException(String msg, Exception reason){
+        super(msg, reason);
+    }
+
+    public RecordException(String msg){
+
+        super(msg);
+    }
 }
