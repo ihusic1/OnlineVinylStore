f65946f19afd1ee5974897acc24dba0f7494cfa2
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 17:55:21 +0200

    ordermanager

--- src/main/java/ba/unsa/etf/rpr/business/OrderManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/OrderManager.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Record;
+import ba.unsa.etf.rpr.domain.Order;
+import ba.unsa.etf.rpr.exceptions.RecordException;
+
+import java.util.List;
+
 public class OrderManager {
+
+    public Order add(Order o) throws RecordException
+    {
+        return DaoFactory.orderDao().add(o);
+    }
+
+    public List<Order> getAll() throws RecordException
+    {
+        return DaoFactory.orderDao().getAll();
+    }
+
 }
