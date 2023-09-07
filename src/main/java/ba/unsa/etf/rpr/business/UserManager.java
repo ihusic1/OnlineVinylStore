3afc2911d397f3db7a58b3ce35729ed8a5159bed
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 17:58:15 +0200

    usermanager

--- src/main/java/ba/unsa/etf/rpr/business/UserManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -1,4 +1,40 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.RecordException;
+import java.util.List;
+
 public class UserManager {
+    public static User findEmail(String emailField) throws RecordException {
+        return DaoFactory.userDao().findEmail(emailField);
+    }
+
+    public void delete(int UserId) throws RecordException {
+        try {
+            DaoFactory.userDao().delete(UserId);
+        } catch (RecordException e) {
+            if (e.getMessage().contains("FOREIGN KEY")) {
+                throw new RecordException("NO");
+            }
+            throw e;
+        }
+
+    }
+    public static User add(User u) throws RecordException {
+        return DaoFactory.userDao().add(u);
+    }
+
+    public User getById(int id) throws RecordException {
+        return DaoFactory.userDao().getById(id);
+    }
+
+    public User update(User u) throws RecordException {
+        return DaoFactory.userDao().update(u);
+    }
+
+    public List<User> getAll() throws RecordException {
+        return DaoFactory.userDao().getAll();
+    }
+
 }
