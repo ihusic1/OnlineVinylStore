128bdb00c064c16c839e39561fb106f3fffeb525
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 18:16:51 +0200

    aboutus final

--- src/main/java/ba/unsa/etf/rpr/controllers/AboutUsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AboutUsController.java
@@ -1,4 +1,16 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class AboutUsController {
+
+    public Button Undo;
+    public void closeWindow(ActionEvent actionEvent) {
+        Stage stage =(Stage)Undo.getScene().getWindow();
+        stage.close();
+    }
 }
