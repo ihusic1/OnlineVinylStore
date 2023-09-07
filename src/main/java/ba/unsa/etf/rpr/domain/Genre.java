64d3018c0cd060c8d0544306c5b98dfe8e3b2375
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 18:00:10 +0200

    genre

--- src/main/java/ba/unsa/etf/rpr/domain/Genre.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Genre.java
@@ -1,4 +1,79 @@
 package ba.unsa.etf.rpr.domain;
+import java.util.Objects;
 
-public class Genre {
+/**
+ *  Show a list of possible categories for genres
+ *
+ * @author Semina Muratović
+ */
+public class Genre implements Idable{
+    private int id;
+    private String name;
+
+
+    public Genre() {
+    }
+
+    public Genre( String name) {
+
+        this.name = name;
+    }
+
+    /**
+     * Getter for id
+     * @return int
+     */
+    public int getId()
+    {
+        return id;
+    }
+
+    /**
+     * Setter for id
+     * @param id int
+     */
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+
+    /**
+     * Getter for name
+     * @return string
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * Setter for name
+     * @param name string
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Genre genre = (Genre) o;
+        return id == genre.id;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(id, name);
+    }
 }
