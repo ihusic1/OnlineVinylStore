31f527f01108532143af28d4fe61db5848e1a2fd
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 17:43:31 +0200

    recorddao

--- src/main/java/ba/unsa/etf/rpr/dao/RecordDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RecordDao.java
@@ -1,4 +1,33 @@
 package ba.unsa.etf.rpr.dao;
 
-public class RecordDao {
+import ba.unsa.etf.rpr.domain.Genre;
+import ba.unsa.etf.rpr.domain.Record;
+import ba.unsa.etf.rpr.exceptions.RecordException;
+
+import java.util.List;
+
+/**
+ * Dao interface for Record domain bean
+ *
+ * @author Ilma Husic
+ */
+public interface RecordDao extends Dao<Record>{
+
+    /**
+     * Returns all records that contains given text.
+     *
+     * @param text search string for records
+     * @return list of records
+     */
+    List<Record> searchByText(String text) throws RecordException;
+
+    /**
+     * Returns all records that contains given text.
+     *
+     * @param genre search string for records
+     * @return list of records
+     */
+    List<Record> searchByGenre(Genre genre) throws RecordException;
+
+
 }
