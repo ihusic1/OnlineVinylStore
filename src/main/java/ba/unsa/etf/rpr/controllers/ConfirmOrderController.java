3149286c124942959d5a077bf77cb97dcea65d72
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 18:46:57 +0200

    confirmordercontroller added

--- src/main/java/ba/unsa/etf/rpr/controllers/ConfirmOrderController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ConfirmOrderController.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class ConfirmOrderController {
+    public Button close;
+
+    public void closeAction(ActionEvent actionEvent) {
+        Stage stage =(Stage)close.getScene().getWindow();
+        stage.close();
+    }
 }
