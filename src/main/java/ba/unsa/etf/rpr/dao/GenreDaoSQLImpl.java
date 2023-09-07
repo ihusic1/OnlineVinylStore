8ed37152586a7b2a0e16c6a3b1d6abfac8763aab
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 15:02:35 +0200

    genredaosql

--- src/main/java/ba/unsa/etf/rpr/dao/GenreDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/GenreDaoSQLImpl.java
@@ -1,4 +1,16 @@
 package ba.unsa.etf.rpr.dao;
 
-public class GenreDaoSQLImpl {
+import ba.unsa.etf.rpr.domain.Genre;
+import ba.unsa.etf.rpr.exceptions.RecordException;
+
+import java.util.List;
+
+/**
+ * Dao interface for Genre domain bean
+ *
+ * @author Ilma Husic
+ */
+
+public interface GenreDao extends Dao<Genre>{
+    List<Genre> searchGenreId(String genreName) throws RecordException;
 }
