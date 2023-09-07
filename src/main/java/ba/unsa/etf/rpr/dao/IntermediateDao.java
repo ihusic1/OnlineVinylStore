d9430547fbdd86dc418e7e621633d5a18528aa2b
Ilma Husić <ihusic1@etf.unsa.ba>
2023-09-07 15:05:04 +0200

    intermediatedao

--- src/main/java/ba/unsa/etf/rpr/dao/IntermediateDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/IntermediateDao.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rpr.dao;
 
-public class IntermediateDao {
+import ba.unsa.etf.rpr.domain.IntermediateTable;
+import ba.unsa.etf.rpr.domain.Record;
+import ba.unsa.etf.rpr.exceptions.RecordException;
+
+import java.util.List;
+
+public interface IntermediateDao extends Dao<IntermediateTable> {
+    List<IntermediateTable> getByUser(int idUser) throws RecordException;
+
 }
